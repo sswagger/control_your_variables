7e22aa594e672bb339da39ce88cb440fd5a9d3f5
Samuel Swagger <fm0776hp@go.minnstate.edu>
2025-11-26 10:25:28 -0600

    updated scripts to use paths based on where they were on computer

--- controlYourVariables/src/Initial.java
+++ controlYourVariables/src/Initial.java
@@ -1,11 +1,16 @@
 import java.io.*;
+import java.nio.file.FileSystems;
 
 public class Initial {
 	public static void main(String[] args) {
 		try {
+			String currPath = System.getProperty("user.dir");
+			String separator = FileSystems.getDefault().getSeparator();
 			ProcessBuilder processBuilder = new ProcessBuilder();
 			if (System.getProperty("os.name").toLowerCase().contains("win")) {
-				processBuilder.command("cmd", "/C", "start cmd.exe -ArgumentList \"/k java -cp \"C:\\Users\\Not Apple\\IdeaProjects\\control_your_variables\\out\\production\\controlYourVariables\" .\\controlYourVariables\\src\\Main.java\""); // Windows
+				String classPath = currPath + separator + "out" + separator + "production" + separator + "controlYourVariables";
+				String javaFile = "." + separator + "controlYourVariables" + separator + "src" + separator + "Main.java";
+				processBuilder.command("cmd", "/C", "start cmd.exe -ArgumentList \"/k java -cp \"" + classPath + "\" \"" + javaFile + "\""); // Windows
 			}
 			processBuilder.start();
 		}
