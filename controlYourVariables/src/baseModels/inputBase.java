bf4a2808300c16937d40827435c00e7f539f0d55
Samuel Swagger <fm0776hp@go.minnstate.edu>
2025-12-22 21:50:11 -0600

    Created jsonReader.java into its own object

--- controlYourVariables/src/baseModels/inputBase.java
+++ controlYourVariables/src/baseModels/inputBase.java
@@ -6,11 +6,6 @@
 package baseModels;
 
 //=== IMPORTED MODULES ===\\
-import org.json.JSONArray;
-import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 //=== CLASS ===\\
@@ -22,7 +17,6 @@ public class inputBase {
 	protected static String dangerColor = "";
 	private static String input = "";
 	private static final Scanner sc = new Scanner(System.in);
-	private static String json;
 
 	//=== FUNCTIONS ===\\
 	/**
@@ -166,70 +160,8 @@ public class inputBase {
 		System.out.println("\033[H\033[2J");
 	}
 
-	/**
-	 * returns data from data.json
-	 *
-	 * @param path {@code String}: The path to data.json
-	 * @param key {@code String}: The key for a list of objects
-	 * @param subKeys {@code ArrayList<String>}: The list that holds the keys of each of the objects
-	 * @return {@code ArrayList<ArrayList<Strings>>}: The list that holds each of the values needed to build an object
-	**/
-	protected static ArrayList<ArrayList<String>> readData(String path, String key, ArrayList<String> subKeys) {
-		ArrayList<ArrayList<String>> data = new ArrayList<>();
-
-		// if data.json has not already been read, read it
-		if (json == null) {
-			readJSON(path);
-		}
-
-		// loop through key values
-		JSONArray jsonKey = new JSONObject(json).getJSONArray(key);
-		for (int i = 0; i < jsonKey.length(); i++) {
-			ArrayList<String> dataSubKeys = new ArrayList<>();
-			// loop through subkeys and add values
-			for (String subKey : subKeys) {
-				JSONObject subJson = jsonKey.getJSONObject(i);
-				dataSubKeys.add(subJson.getString(subKey));
-			}
-			data.add(dataSubKeys);
-		}
-		return data;
-	}
-
-	/**
-	 * returns data from data.json
-	 *
-	 * @param path {@code String}: The path to data.json
-	 * @param key {@code String}: The key for the needed value
-	 * @return {@code int}: The value for the key
-	 **/
-	protected static int readData(String path, String key) {
-		// if data.json has not already been read, read it
-		if (json == null) {
-			readJSON(path);
-		}
-
-		// return integer value for key
-		return new JSONObject(json).getInt(key);
-	}
 
 	//=== PRIVATE FUNCTIONS ===\\
-	// read data.json
-	private static void readJSON(String path) {
-		// read JSON and create object
-		try (BufferedReader in = new BufferedReader(new FileReader(path + "data.json"))) {
-			String line;
-			StringBuilder sbJSON = new StringBuilder();
-
-			while ((line = in.readLine()) != null) {
-				sbJSON.append(line.trim());
-			}
-			json = sbJSON.toString();
-		}
-		catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
 	// display input and ensure that the user said something
 	private static void getInput(String prompt) {
 		System.out.print(inputColor + prompt + ":" + neutral + "  ");
