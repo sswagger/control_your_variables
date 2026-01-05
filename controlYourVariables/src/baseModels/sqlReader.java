aff06cb398f9570c1465a02cb8d6c49ca579f009
Samuel Swagger <fm0776hp@go.minnstate.edu>
2026-01-04 18:20:39 -0600

    finished adding sqlite

--- controlYourVariables/src/baseModels/sqlReader.java
+++ controlYourVariables/src/baseModels/sqlReader.java
@@ -5,13 +5,20 @@ import java.sql.SQLException;
 
 public class sqlReader {
 	public void test() {
-		String url = "jdbc:sqlite:\\..\\myFarm\\Data\\myFarm.db";
+		try {
+			Class.forName("org.sqlite.JDBC");
+		}
+		catch (ClassNotFoundException e) {
+			System.out.println(e);
+		}
+
+		String url = "jdbc:sqlite:C:\\Users\\Not Apple\\IdeaProjects\\control_your_variables\\controlYourVariables\\src\\myFarm\\Data\\myFarm.db";
 
 		try (var conn = DriverManager.getConnection(url)) {
 			if (conn != null) {
-				var meta = conn.getMetaData();
-				System.out.println("The driver name is " + meta.getDriverName());
-				System.out.println("A new database has been created.");
+//				var meta = conn.getMetaData();
+//				System.out.println("The driver name is " + meta.getDriverName());
+//				System.out.println("A new database has been created.");
 			}
 		} catch (SQLException e) {
 			System.err.println(e.getMessage());
