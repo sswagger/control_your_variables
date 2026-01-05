a19b4dd89a5c45a0c9218fb16122e23586403f20
Samuel Swagger <fm0776hp@go.minnstate.edu>
2026-01-04 23:11:44 -0600

    toyed with sqlite db

--- controlYourVariables/src/baseModels/sqlReader.java
+++ controlYourVariables/src/baseModels/sqlReader.java
@@ -1,27 +1,55 @@
 package baseModels;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 
 public class sqlReader {
-	public void test() {
+	private String url;
+
+	public sqlReader(String dataPath) {
 		try {
 			Class.forName("org.sqlite.JDBC");
 		}
 		catch (ClassNotFoundException e) {
-			System.out.println(e);
+			return;
 		}
 
-		String url = "jdbc:sqlite:C:\\Users\\Not Apple\\IdeaProjects\\control_your_variables\\controlYourVariables\\src\\myFarm\\Data\\myFarm.db";
+		this.url = "jdbc:sqlite:" + dataPath;
+	}
 
-		try (var conn = DriverManager.getConnection(url)) {
-			if (conn != null) {
-//				var meta = conn.getMetaData();
-//				System.out.println("The driver name is " + meta.getDriverName());
-//				System.out.println("A new database has been created.");
+	public ArrayList<String> readDb(String sql, ArrayList<String> columns) throws Exception {
+		ArrayList<String> data = new ArrayList<>();
+		try (var connection = DriverManager.getConnection(this.url)) {
+			String readQuery = sql;
+			PreparedStatement preparedStatement = connection.prepareStatement(readQuery);
+			ResultSet s = preparedStatement.executeQuery();
+
+			for (String i : columns) {
+				data.add(s.getString(i));
 			}
-		} catch (SQLException e) {
-			System.err.println(e.getMessage());
+
+			preparedStatement.close();
+			return data;
+		}
+		catch (SQLException e) {
+			throw new Exception(e.getMessage());
+		}
+	}
+
+	public void updateDb(String sql) throws Exception {
+		try (var connection = DriverManager.getConnection(this.url)) {
+			String updateQuery = sql;
+			PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
+			preparedStatement.executeUpdate();
+
+			preparedStatement.close();
+		}
+		catch (SQLException e) {
+			throw new Exception(e.getMessage());
 		}
 	}
 }
